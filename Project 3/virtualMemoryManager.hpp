35d8a37bd46ea328bd3d1fa234fa6164f30e3310
nrgapple <champtoday@me.com>
2017-04-18 12:20:33 -0400

    Sets up Project

---
+++ Project 3/virtualMemoryManager.hpp
@@ -0,0 +1,20 @@
+//
+//  virtualMemoryManager.cpp
+//  Project 3
+//
+//  Created by Adam Geiger on 4/18/17.
+//  Copyright © 2017 Adam Geiger. All rights reserved.
+//
+
+#include <stdio.h>
+#include "virtualMemoryManagerInterface"
+
+class virtualMemoryManager: protected virtualMemoryManagerInterface
+{
+  
+  unsigned long long memoryAccess(unsigned long long address)
+  {
+    
+  }
+};
+
