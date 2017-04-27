16ba80ac0831dc3312d131ad38c58ca605da95f8
nrgapple <champtoday@me.com>
2017-04-27 11:35:01 -0400

    actually changes memoryManager

--- Project 3/main.cpp
+++ Project 3/main.cpp
@@ -7,9 +7,20 @@
 //
 
 #include <iostream>
+#include "memoryManager.hpp"
 
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  std::cout << "Hello, World!\n";
+  
+    /*
+     * N = Page Address bit length, 2^N = Page Size
+     * numFrames = Number of Frames in Physical Memory, 1 Frame holds 1 Page
+     * Total size of Physical Memory = numFrames * 2^N
+     * virtualAddressSpace = Address bit length of Virtual Memory
+     * Total size of Virtual Memory = 2^virtualAddressSpace
+     * Replacememtn Policies = FIFO, LRU
+     */
+  
+    
+  
     return 0;
 }
