faa9582e40e3f2ff08d9bb6a5d9f9d66fe94e21c
Collin <demattcp@gmail.com>
2017-05-03 19:31:00 -0400

    more formatting!!

--- Project 3/main.cpp
+++ Project 3/main.cpp
@@ -16,53 +16,55 @@ ReplacementPolicy POLICY = ReplacementPolicy::FIFO;
 
 int main(int argc, char* argv[])
 {
-  memoryManager vmmi = memoryManager(POLICY, BITS_PER_BLOCK, NUMBER_OF_PAGES,
-                                     VIRT_ADDRESS_SPACE);
+    memoryManager vmmi = memoryManager(POLICY, BITS_PER_BLOCK, NUMBER_OF_PAGES,
+                                       VIRT_ADDRESS_SPACE);
 
-  cout << "Alexandar Devic - Cauchy Distribution Memory Generator" << endl;
-  cout << "Input the number of memory requests you want to process[0, ]: ";
-  unsigned long int operations = 0;
-  cin >> operations;
-  cout << "Input the average place in virtual memory where you want requests" <<
-       " to occur[0, ]: ";
-  unsigned long int averageVirtLoc = 0;
-  cin >> averageVirtLoc;
-  cout << "Input the standard deviation for the distribution[0, ]: ";
-  unsigned long int stDeviation = 0;
-  cin >> stDeviation;
+    cout << "Alexandar Devic - Cauchy Distribution Memory Generator" << endl;
+    cout << "Input the number of memory requests you want to process[0, ]: ";
+    unsigned long int operations = 0;
+    cin >> operations;
+    cout << "Input the average place in virtual memory where you want requests"
+         << " to occur[0, ]: ";
+    unsigned long int averageVirtLoc = 0;
+    cin >> averageVirtLoc;
+    cout << "Input the standard deviation for the distribution[0, ]: ";
+    unsigned long int stDeviation = 0;
+    cin >> stDeviation;
 
-  cout << "Press [RETURN] to begin processing";
-  cin.get(); cin.get();
+    cout << "Press [RETURN] to begin processing";
+    cin.get(); cin.get();
 
-  cout << "Creating random engine generator...\n";
-  default_random_engine generator;
+    cout << "Creating random engine generator...\n";
+    default_random_engine generator;
 
-  cout << "Generating Cauchy Distriburion...\n";
-  cauchy_distribution<double> distribution(averageVirtLoc, stDeviation);
+    cout << "Generating Cauchy Distriburion...\n";
+    cauchy_distribution<double> distribution(averageVirtLoc, stDeviation);
 
-  cout << "Starting to produce random memory requests...\n\n";
-  unsigned long int currentProcess = 0;
-  unsigned long int currSwaps = vmmi.numberPageSwaps();
-  for (int i = 0; i < operations; i++) {
-    unsigned long int number = distribution(generator);
-    while (number < 0 && number >= pow(2, VIRT_ADDRESS_SPACE))
-      number = distribution(generator);
-    unsigned long int whatsReturned = vmmi.memoryAccess(number);
+    cout << "Starting to produce random memory requests...\n\n";
+    unsigned long int currentProcess = 0;
+    unsigned long int currSwaps = vmmi.numberPageSwaps();
 
-    if (!PRINT_IN_HEX)
-      cout << "Virtual [" << number << "] is in Physical[" << whatsReturned <<
-           "] " << (currSwaps != vmmi.numberPageSwaps() ? "SWAPPED" : "") <<
-           endl;
-    else
-      cout << "Virtual [" << std::hex << number << "] is in Physical[" <<
-           std::hex << whatsReturned << "] " <<
-           (currSwaps != vmmi.numberPageSwaps() ? "SWAPPED" : "") << endl;
+    for (int i = 0; i < operations; i++)
+    {
+        unsigned long int number = distribution(generator);
+        while (number < 0 && number >= pow(2, VIRT_ADDRESS_SPACE))
+            number = distribution(generator);
+        unsigned long int whatsReturned = vmmi.memoryAccess(number);
 
-    currSwaps = vmmi.numberPageSwaps();
-  }
+        if (!PRINT_IN_HEX)
+            cout << "Virtual [" << number << "] is in Physical[" <<
+                 whatsReturned << "] " << (currSwaps != vmmi.numberPageSwaps()
+                                           ? "SWAPPED" : "") << endl;
+        else
+            cout << "Virtual [" << std::hex << number << "] is in Physical[" <<
+                 std::hex << whatsReturned << "] " <<
+                 (currSwaps != vmmi.numberPageSwaps() ? "SWAPPED" : "") << endl;
 
-  cout << "Finished the test! Press [ENTER] to close... ";
-  cin.get();
+        currSwaps = vmmi.numberPageSwaps();
+    }
 
-  return 0;
+    cout << "Finished the test! Press [ENTER] to close... ";
+    cin.get();
+
+    return 0;
 }
